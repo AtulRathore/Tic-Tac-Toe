67d03136a626bcc13d76cbed4f3583b50ba2561a
Atul Rathore <kshtriya.atul@gmail.com>
2017-01-17 04:53:20 +0530

    First

---
+++ app/src/main/java/com/example/atul/tic_tac_toe/MainActivity.java
@@ -0,0 +1,27 @@
+package com.example.atul.tic_tac_toe;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+
+public class MainActivity extends AppCompatActivity {
+
+    public void dropIn(View view){
+
+        ImageView counter = (ImageView) view;
+
+        counter.setTranslationY(-1000f);
+
+        counter.setImageResource(R.drawable.yellow);
+
+        counter.animate().translationYBy(1000f).rotation(360).setDuration(300);
+
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
